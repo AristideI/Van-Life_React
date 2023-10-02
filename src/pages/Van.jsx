2c861b7106fe21b1d266d302c9776578e13e7123
AristideI <a.isingizwe@alustudent.com>
2023-10-02 23:17:06 +0200

    updates

--- src/pages/Van.jsx
+++ src/pages/Van.jsx
@@ -1,1 +1,49 @@
-export default function Van() {}
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export default function Van() {
+  const params = useParams();
+  let [van, setVan] = useState(null);
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
+
+  return (
+    <div>
+      <section className="px-[25%] my-12">
+        {van ? (
+          <section className="flex flex-col gap-8">
+            <img
+              className="w-full h-[30rem] object-cover"
+              src={van.imageUrl}
+              alt=""
+            />
+            <p
+              className=" font-semibold text-2xl text-white rounded-lg w-fit px-12 py-2"
+              style={{
+                background:
+                  van.type === "simple"
+                    ? "#E17654"
+                    : van.type === "rugged"
+                    ? "#115E59"
+                    : "black",
+              }}
+            >
+              {van.type[0].toUpperCase() + van.type.slice(1)}
+            </p>
+            <p className="font-bold text-4xl">{van.name}</p>
+            <p className="font-semibold text-2xl">${van.price}/day</p>
+            <p className="font-semibold text-xl">{van.description}</p>
+            <p className="font-bold text-white bg-[#E17654] py-2 text-center text-2xl rounded-lg">
+              Rent This Van
+            </p>
+          </section>
+        ) : (
+          <p className="font-semibold text-4xl">Loading.....</p>
+        )}
+      </section>
+    </div>
+  );
+}
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,17 +1,41 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Vans() {
-  let [vans, setVans] = useState(null);
-  console.log(vans);
+  let [vans, setVans] = useState([]);
   useEffect(() => {
     fetch("/api/vans")
       .then((resp) => resp.json())
       .then((data) => setVans(data.vans));
   }, []);
-
-  const disp = vans.map((elt) => {
-    return <section>Pro</section>;
+  const disp = vans.map((elt, ind) => {
+    return (
+      <Link to={`/vans/${elt.id}`} key={ind} className="flex flex-col gap-2">
+        <img
+          className="w-full h-96 object-cover rounded-xl"
+          src={elt.imageUrl}
+          alt="Vans"
+        />
+        <p className="font-bold text-2xl ">{elt.name}</p>
+        <p className="font-medium text-xl">${elt.price}/Day</p>
+        <p
+          className=" font-semibold text-white rounded-lg w-fit px-5 py-1"
+          style={{
+            background:
+              elt.type === "simple"
+                ? "#E17654"
+                : elt.type === "rugged"
+                ? "#115E59"
+                : "black",
+          }}
+        >
+          {elt.type[0].toUpperCase() + elt.type.slice(1)}
+        </p>
+      </Link>
+    );
   });
 
-  return <section>{disp}</section>;
+  return (
+    <section className="px-[25%] mt-12 grid grid-cols-2 gap-8">{disp}</section>
+  );
 }
